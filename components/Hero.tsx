8f4a65bd428d12932a4e3c694daee5665b154693
Maria Starostina <maria.starostina.work@gmail.com>
2024-07-31 18:22:32 +0300

    Add title TextGenerateEffect
    Change text titles, size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/text-generate-effect'
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute stop-0 left-0">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
@@ -18,6 +19,16 @@ const Hero = () => {
           <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
             Dynamic Web with Next.js
           </h2>
+
+          <TextGenerateEffect 
+            className='text-center text-[40px] md:text-5xl lg:text-6xl'
+            words='Transforming Concepts into Seamless User Experiences'
+          />
+
+          <p className ="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Maria, a Next.js developer based in Moscow
+          </p>
+
         </div>
       </div>
 

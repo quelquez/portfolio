2479d406b55545073d6c12479de906ecb4a6c117
Maria Starostina <maria.starostina.work@gmail.com>
2024-08-01 23:20:44 +0300

    Add BentoGrid
    Add index.ts to contain data

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const gridItems =[
+    {title: 'Title1', description:'Desc1', id:1},
+    {title: 'Title2', description:'Des2c1', id:1},
+    {title: 'Titl2e1', description:'Deцsc1', id:1},
+    {title: '2Title1', description:'Desc21', id:4},
+    {title: 'Titleц1', description:'2eцsc1', id:5},
+]
